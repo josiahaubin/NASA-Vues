13bad1caa1846c02ace5cea07bd4f03d9fa438c6
Josiah Aubin <josiahaubin@gmail.com>
2019-09-10 13:49:35 -0600

    Basic set up, NASA Image linked to home

---
+++ src/components/NasaImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="NasaImage"></div>
+</template>
+
+
+<script>
+export default {
+  name: "NasaImage",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <NasaImage />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import NasaImage from "../components/NasaImage";
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    NasaImage
   }
-}
+};
 </script>
