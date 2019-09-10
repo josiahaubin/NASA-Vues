650c1d6fc5ce3a7d3a50be94355c406913a35ab5
Josiah Aubin <josiahaubin@gmail.com>
2019-09-10 14:11:19 -0600

    Button and title for APOD

--- src/components/NasaImage.vue
+++ src/components/NasaImage.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="NasaImage"></div>
+  <div class="NasaImage col-12 mt-5">
+    <h3>NASA APOD</h3>
+    <button class="btn btn-success" @click="getApod()">Get APOD</button>
+  </div>
 </template>
 
 
@@ -10,7 +13,9 @@ export default {
     return {};
   },
   computed: {},
-  methods: {},
+  methods: {
+    getApod() {}
+  },
   components: {}
 };
 </script>
